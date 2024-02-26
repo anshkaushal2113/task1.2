a961fb096f100edfcfb29bd722e0c657828e8331
anshkaushal2113 <ansh2113.be23@chitkara.edu.in>
2024-02-26 15:19:17 +0530

    commitin calculator

---
+++ cal.py
@@ -0,0 +1,16 @@
+
+a = input('enter operator')
+b = input('enter 1st no.')
+c = input('enter 2nd no.')
+if a =='+':
+    print(a+b)
+elif a=='-':
+    print(a-b)
+elif a == '*':
+    print(a*b)
+elif a == '/':
+    print(a/b)
+elif a == '%':
+    print(a%b)
+
+
